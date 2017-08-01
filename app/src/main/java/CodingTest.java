350762134ca376233903a6f3e14dbacc254dc371
Gallus Awa <Gallus>
2017-08-01 16:16:46 -0400

    Initial commit

---
+++ app/src/main/java/CodingTest.java
@@ -0,0 +1,59 @@
+/**
+ * Created by HP on 8/1/2017.
+ */
+
+public class CodingTest {
+    public static main(String[] args){
+        int num 5, num 6, num 7,num 8;
+
+   //     if ()
+            public void printnumber(ArrayList<integer> , num){
+
+            int [] numbers = new int { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
+
+            if(numbers/3=0) {
+                System.out.println("Fizz");
+            } else if (numbers/5 =0) {
+                System.out.println("Buzz");
+            }
+                else if (numbers/2 && numbers/5){
+            System.out.println("FizzBuzz")
+                }
+                for (int i=0, i< numbers.lenght, i++){
+              System.out.println(numbers[i]);
+        }
+
+        //problem2/
+
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        stringArrayList.add("A");
+        stringArrayList.add("B");
+        stringArrayList.add("A");
+        stringArrayList.add("C");
+        stringArrayList.add("D");
+        stringArrayList.add("B");
+        stringArrayList.add("E");
+
+        string charA, charB = '';
+        if (charA = charB) {
+            System.out.println("We have a duplicate");
+        }
+        else
+        {
+            System.out.println("There is not duplicate");''
+        }
+
+
+   //         int [] arrayList = new arraylist[10];
+   //     for(int 1=0, i< arraylist.lenght, i++)
+    //        {
+            arraylist('0');
+            arraylist.add('1');
+                num3  = arraylist.add ('2);
+
+
+
+
+        }
+    }
+}
